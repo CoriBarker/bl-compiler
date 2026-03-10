85918e5c3ef2f109583c7f414e7d604df0272c03
Cori Barker <coribarker2@gmail.com>
2026-03-10 15:13:23 +0000

    refactored types

--- include/type.hpp
+++ include/type.hpp
@@ -1,6 +1,6 @@
 #pragma once
 
-enum struct Type {
+enum struct TypeKind {
     INT,
     CHAR,
     BOOL,
@@ -8,4 +8,28 @@ enum struct Type {
     FLOAT,
     DOUBLE,
     VOID,
+    FUNCTION,
+};
+
+struct Type {
+    TypeKind kind;
+
+    Type* return_type = nullptr;
+    std::vector<Type*> param_types;
+
+    static Type makeInt() { return { TypeKind::INT }; }
+    static Type makeChar() { return { TypeKind::CHAR }; }
+    static Type makeBool() { return { TypeKind::BOOL }; }
+    static Type makeString() { return { TypeKind::STRING }; }
+    static Type makeFloat() { return { TypeKind::FLOAT }; }
+    static Type makeDouble() { return { TypeKind::DOUBLE }; }
+    static Type makeVoid() { return { TypeKind::VOID }; }
+
+    static Type makeFunction(Type* return_type, std::vector<Type*> param_types) {
+	Type t;
+	t.kind = TypeKind::FUNCTION;
+	t.return_type = return_type;
+	t.param_types = std::move(param_types);
+	return t;
+    };
 };
