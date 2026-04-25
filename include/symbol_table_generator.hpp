c88542b36f0fea5a78635d7876743f2883fda98e
Cori Barker <coribarker2@gmail.com>
2026-04-25 22:52:00 +0100

    feat: implement symbol table generator
    Walks the AST and performs scope analysis:
    - Two-pass function registration so all functions are visible to each other
    - Unique scope names using counter (main::if_0, main::for_1, etc.)
    - Validates undefined variables, duplicate declarations, and function calls
    - Collects all errors in a single pass rather than stopping at first error

---
+++ include/symbol_table_generator.hpp
@@ -0,0 +1,33 @@
+#pragma once
+
+#include "symbol_table.hpp"
+#include "ast_node.hpp"
+
+#include <string>
+#include <vector>
+
+class SymbolTableGenerator {
+public:
+    explicit SymbolTableGenerator();
+    SymbolTable generate(ProgramNode* program);
+
+    std::vector<std::string> getErrors() const { return errors; }
+   
+private:
+    SymbolTable table;
+    std::vector<std::string> errors;
+    int scope_counter;
+
+    void visitProgram(ProgramNode* node);
+    void visitFunctionDeclaration(FunctionDeclarationNode* node);
+    void visitStatement(ASTNode* node);
+    void visitVariableDeclaration(VariableDeclarationNode* node);
+    void visitVariableAssignment(VariableAssignmentNode* node);
+    void visitIfStatement(IfStatementNode* node);
+    void visitWhileStatement(WhileStatementNode* node);
+    void visitForStatement(ForStatementNode* node);
+    void visitExpression(ASTNode* node);
+    void visitFunctionCall(FunctionCallNode* node);
+
+    void error(const std::string& message, int line, int column);
+};
---
+++ src/symbol_table_generator.cpp
@@ -0,0 +1,265 @@
+#include "symbol_table_generator.hpp"
+
+#include <iostream>
+
+SymbolTableGenerator::SymbolTableGenerator() : scope_counter(0) {}
+
+SymbolTable SymbolTableGenerator::generate(ProgramNode* program) {
+    table.enterScope("global");
+
+    visitProgram(program);
+
+    return std::move(table);
+}
+
+void SymbolTableGenerator::visitProgram(ProgramNode* node) {
+    for (int i = 0; i < (int)node->function_declarations.size(); i++) {
+        auto& function = node->function_declarations[i];
+        std::string name = function->identifier;
+        std::string qualified_name = "global::" + name;
+        
+        Symbol symbol;
+        symbol.name = name;
+        symbol.qualified_name = qualified_name;
+        symbol.kind = SymbolKind::FUNCTION;
+        symbol.type = function->return_type;
+        symbol.line = function->line;
+        symbol.column = function->column;
+
+        for (int j = 0; j < (int)function->parameters.size(); j++) {
+            symbol.parameter_types.push_back(function->parameters[j]->type);
+        }
+
+        table.define(symbol);
+    }
+
+    for (int i = 0; i < (int)node->function_declarations.size(); i++) {
+        visitFunctionDeclaration(node->function_declarations[i].get());
+    }
+}
+
+void SymbolTableGenerator::visitFunctionDeclaration(FunctionDeclarationNode* node) {
+    table.enterScope(node->identifier);
+
+    for (int i = 0; i < (int)node->parameters.size(); i++) {
+        auto& parameter = node->parameters[i];
+        std::string name = parameter->identifier;
+        std::string qualified_name = node->identifier + "::" + parameter->identifier;
+
+        Symbol symbol;
+        symbol.name = name;
+        symbol.qualified_name = qualified_name;
+        symbol.kind = SymbolKind::PARAMETER;
+        symbol.type = parameter->type;
+        symbol.line = parameter->line;
+        symbol.column = parameter->column;
+
+        table.define(symbol);
+    }
+
+    for (int i = 0; i < (int)node->body.size(); i++) {
+        visitStatement(node->body[i].get());
+    }
+
+    table.exitScope();
+}
+
+void SymbolTableGenerator::visitStatement(ASTNode* node) {
+    if (auto p = dynamic_cast<VariableDeclarationNode*>(node)) {
+        visitVariableDeclaration(p);
+    }
+
+    else if (auto p = dynamic_cast<VariableAssignmentNode*>(node)) {
+        visitVariableAssignment(p);
+    }
+
+    else if (auto p = dynamic_cast<IfStatementNode*>(node)) {
+        visitIfStatement(p);
+    }
+
+    else if (auto p = dynamic_cast<WhileStatementNode*>(node)) {
+        visitWhileStatement(p);
+    }
+    
+    else if (auto p = dynamic_cast<ForStatementNode*>(node)) {
+        visitForStatement(p);
+    }
+
+    else if (auto p = dynamic_cast<ReturnNode*>(node)) {
+        if (p->expression) {
+            visitExpression(p->expression.get());
+        }
+    }
+}
+
+void SymbolTableGenerator::visitVariableDeclaration(VariableDeclarationNode* node) {
+    if (table.lookupCurrent(node->identifier)) {
+        error("'" + node->identifier + "' is already declared in this scope", node->line, node->column);
+        return;
+    }
+
+    std::string qualified_name = table.scopes.back()->scope_name + "::" + node->identifier;
+
+    Symbol symbol;
+    symbol.name = node->identifier;
+    symbol.qualified_name = qualified_name;
+    symbol.kind = SymbolKind::VARIABLE;
+    symbol.type = node->type;
+    symbol.line = node->line;
+    symbol.column = node->column;
+
+    table.define(symbol);
+
+    if (node->value) {
+        visitExpression(node->value.get());
+    }
+}
+
+void SymbolTableGenerator::visitVariableAssignment(VariableAssignmentNode* node) {
+    if (!table.lookup(node->identifier)) {
+        error("'" + node->identifier + "' is undefined", node->line, node->column);
+        return;
+    }
+
+    visitExpression(node->value.get());
+}
+
+void SymbolTableGenerator::visitIfStatement(IfStatementNode* node) {
+    visitExpression(node->expression.get());
+
+    std::string scope_name = table.scopes.back()->scope_name + "::if_" + std::to_string(scope_counter++);
+    table.enterScope(scope_name);
+    
+    for (int i = 0; i < (int)node->body.size(); i++) {
+        visitStatement(node->body[i].get());
+    }
+
+    table.exitScope();
+
+    if (node->else_statement) {
+        std::string else_scope_name = table.scopes.back()->scope_name + "::if_else" + std::to_string(scope_counter++);
+        table.enterScope(else_scope_name);
+
+        for (int i = 0; i < (int)node->else_statement->body.size(); i++) {
+            visitStatement(node->else_statement->body[i].get());
+        }
+
+        table.exitScope();
+    }
+}
+
+void SymbolTableGenerator::visitWhileStatement(WhileStatementNode* node) {
+    visitExpression(node->expression.get());
+
+    std::string scope_name = table.scopes.back()->scope_name + "::while_" + std::to_string(scope_counter++);
+    table.enterScope(scope_name);
+
+    for (int i = 0; i < (int)node->body.size(); i++) {
+        visitStatement(node->body[i].get());
+    }
+
+    table.exitScope();
+}
+
+void SymbolTableGenerator::visitForStatement(ForStatementNode* node) {
+    std::string scope_name = table.scopes.back()->scope_name + "::for_" + std::to_string(scope_counter++);
+    table.enterScope(scope_name);
+
+    if (node->init) {
+        if (!node->init->identifier.empty() && node->init->type != Type::VOID) {
+            std::string name = node->init->identifier;
+            std::string qualified_name = table.scopes.back()->scope_name + "::" + name;
+
+            Symbol symbol;
+            symbol.name = name;
+            symbol.qualified_name = qualified_name;
+            symbol.kind = SymbolKind::VARIABLE;
+            symbol.type = node->init->type;
+            symbol.line = node->init->line;
+            symbol.column = node->init->column;
+
+            table.define(symbol);
+
+            if (node->init->expression) {
+                visitExpression(node->init->expression.get());
+            }
+        }
+
+        else {
+            if (!table.lookup(node->init->identifier)) {
+                error("'" + node->init->identifier + "' is undefined", node->init->line, node->init->column);
+            }
+
+            if (node->init->expression) {
+                visitExpression(node->init->expression.get());
+            }
+        }
+    }
+
+    if (node->condition) {
+        visitExpression(node->condition.get());
+    }
+
+    if (node->update) {
+        visitStatement(node->update.get());
+    }
+
+    for (int i = 0; i < (int)node->body.size(); i++) {
+        visitStatement(node->body[i].get());
+    }
+
+    table.exitScope();
+}
+
+void SymbolTableGenerator::visitExpression(ASTNode* node) {
+    if (!node) return;
+    
+    if (auto p = dynamic_cast<IdentifierNode*>(node)) {
+        if (!table.lookup(p->value)) {
+            error("'" + p->value + "' is undefined", p->line, p->column);
+        }
+    }
+
+    else if (auto p = dynamic_cast<FunctionCallNode*>(node)) {
+        visitFunctionCall(p);
+    }
+
+    else if (auto p = dynamic_cast<BinaryOperationNode*>(node)) {
+        visitExpression(p->left.get());
+        visitExpression(p->right.get());
+    }
+
+    else if (auto p = dynamic_cast<UnaryOperationNode*>(node)) {
+        visitExpression(p->operand.get());
+    }
+}
+
+void SymbolTableGenerator::visitFunctionCall(FunctionCallNode* node) {
+    auto symbol = table.lookup(node->identifier);
+
+    if (symbol == nullptr) {
+        error("'" + node->identifier + "' is not defined", node->line, node->column);
+        return;
+    }
+
+    if (symbol->kind != SymbolKind::FUNCTION) {
+        error("'" + node->identifier + "' is not a function", node->line, node->column);
+        return;
+    }
+    
+    if (node->arguments.size() != symbol->parameter_types.size()) {
+        error("'" + node->identifier + "' expects " + std::to_string(symbol->parameter_types.size()) + " arguments but got " + std::to_string(node->arguments.size()), node->line, node->column);
+    }
+
+    for (int i = 0; i < (int)node->arguments.size(); i++) {
+        visitExpression(node->arguments[i].get());
+    }
+}
+
+void SymbolTableGenerator::error(const std::string& message, int line, int column) {
+    std::string err;
+    err += "ERROR: " + message + "\n";
+    err += "       at line " + std::to_string(line) + ", column " + std::to_string(column) + "\n";
+ 
+    errors.push_back(err);
+}
