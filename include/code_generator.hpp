27e52ac8da694316eac1eec4badc15897e889bdb
Cori Barker <coribarker2@gmail.com>
2026-04-27 14:41:40 +0100

    feat(code-gen): added CodeGeneration header file

---
+++ include/code_generator.hpp
@@ -0,0 +1,37 @@
+#pragma once
+
+#include "symbol_table.hpp"
+
+class CodeGenerator {
+public:
+    CodeGenerator(SymbolTable& table);
+    void generate(ProgramNode* node, const std::string& filename);
+    
+private:
+    SymbolTable& table;
+    std::ofstream output;
+    int label_counter;
+    std::string current_function;
+    std::unordered_map<std::string, int> offsets;
+    int current_offset;
+
+    void emit(const std::string& line);
+    void emitLabel(const std::string& label);
+    std::string newLabel(const std::string& prefix);
+
+    void builOffsetMap(const std::string& function_name);
+    int getOffset(const std::string& name);
+
+    void generateFunction(FunctionDeclarationNode* node);
+    void generateStatement(ASTNode* node);
+    void generateVariableDeclaration(VariableDeclarationNode* node);
+    void generateVariableAssignment(VariableAssignmentNode* node);
+    void generateReturn(ReturnNode* node);
+    void generateIf(IfStatementNode* node);
+    void generateWhile(WhileStatementNode* node);
+    void generateFor(ForStatementNode* node);
+    void generateExpression(ASTNode* node);
+    void generateBinaryOp(BinaryOperationNode* node);
+    void generateUnaryOp(UnaryOperationNode* node);
+    void generateFunctionCall(FunctionCallNode* node);
+};
