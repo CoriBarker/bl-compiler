3c5761d0ad66186addbe13665e861d6b0ab4df23
Cori Barker <coribarker2@gmail.com>
2026-04-30 22:04:42 +0100

    feat(array): array AST nodes generated by parser

--- include/ast_node.hpp
+++ include/ast_node.hpp
@@ -18,6 +18,7 @@ class ParameterNode : public ASTNode {
 public:
     Type type;
     std::string identifier;
+    bool is_array = false;
 
     ParameterNode() {}
 };
@@ -27,6 +28,7 @@ public:
     std::string identifier;
     std::vector<std::unique_ptr<ParameterNode>> parameters;
     Type return_type;
+    bool return_array = false;
     std::vector<std::unique_ptr<ASTNode>> body;
 
     FunctionDeclarationNode() {}
@@ -108,6 +110,40 @@ public:
     std::string value;
 
     BooleanLiteralNode(const std::string& value) : value(value) {}
+};
+
+class ArrayLiteralNode : public ASTNode {
+public:
+    std::vector<std::unique_ptr<ASTNode>> value;
+
+  ArrayLiteralNode(std::vector<std::unique_ptr<ASTNode>> value) : value(std::move(value)) {}
+};
+
+class ArrayDeclarationNode : public ASTNode {
+public:
+    std::string identifier;
+    Type element_type;
+    std::unique_ptr<ASTNode> size_expr = nullptr;
+    std::unique_ptr<ArrayLiteralNode> elements = nullptr;
+
+    ArrayDeclarationNode() {}
+};
+
+class ArrayAccessNode : public ASTNode {
+public:
+    std::string identifier;
+    std::unique_ptr<ASTNode> index;
+
+    ArrayAccessNode() {}
+};
+
+class ArrayAssignmentNode : public ASTNode {
+public:
+    std::string identifier;
+    std::unique_ptr<ASTNode> index;
+    std::unique_ptr<ASTNode> value;
+
+    ArrayAssignmentNode() {}
 };
 
 class ElseStatementNode : public ASTNode {
