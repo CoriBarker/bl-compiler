e78ca371cc0d5dc8e4c123f620674e7956e8c84c
Cori Barker <coribarker2@gmail.com>
2026-05-04 21:40:19 +0100

    feat(semantic): implemented semantic analysis for arrays and operations

--- include/semantic_analyser.hpp
+++ include/semantic_analyser.hpp
@@ -26,17 +26,23 @@ private:
     SymbolTable& table;
     std::vector<std::string> errors;
 
-    void analyseFunction(FunctionDeclarationNode* node, Context& ctx);
+    void analyseFunction(FunctionDeclarationNode* node);
     void analyseStatement(ASTNode* node, Context& ctx);
     void analyseVariableDeclaration(VariableDeclarationNode* node, Context& ctx);
+    void analyseArrayDeclaration(ArrayDeclarationNode* node, Context& ctx);
     void analyseVariableAssignment(VariableAssignmentNode* node, Context& ctx);
+    void analyseArrayAssignment(ArrayAssignmentNode* node, Context& ctx);
     void analyseReturn(ReturnNode* node, Context& ctx);
     void analyseIf(IfStatementNode* node, Context& ctx);
     void analyseWhile(WhileStatementNode* node, Context& ctx);
     void analyseFor(ForStatementNode* node, Context& ctx);
-    void analyseFunctionCall(FunctionCallNode* node, Context& ctx);
     void analyseExpression(ASTNode* node, Context& ctx);
+    void analyseFunctionCall(FunctionCallNode* node, Context& ctx);
     void analyseIdentifier(IdentifierNode* node, Context& ctx);
+    void analyseBinOp(BinaryOperationNode* node, Context& ctx);
+    void analyseUnaryOp(UnaryOperationNode* node, Context& ctx);
+    void analyseArrayAccess(ArrayAccessNode* node, Context& ctx);
+    void analyseArrayLiteral(ArrayLiteralNode* node, Context& ctx);
 
     bool allPathsReturn(std::vector<std::unique_ptr<ASTNode>>& nodes);
 
--- src/semantic_analyser.cpp
+++ src/semantic_analyser.cpp
@@ -3,13 +3,13 @@
 SemanticAnalyser::SemanticAnalyser(SymbolTable& table) : table(table) {}
 
 void SemanticAnalyser::analyse(ProgramNode* node) {
-    Context ctx;
     for (auto& it : node->function_declarations) {
-        analyseFunction(it.get(), ctx);
+        analyseFunction(it.get());
     }
 }
 
-void SemanticAnalyser::analyseFunction(FunctionDeclarationNode* node, Context& ctx) {
+void SemanticAnalyser::analyseFunction(FunctionDeclarationNode* node) {
+    Context ctx;
     ctx.name = node->identifier;
     ctx.return_type = node->return_type;
 
@@ -54,17 +54,41 @@ void SemanticAnalyser::analyseVariableDeclaration(VariableDeclarationNode* node,
     }
 }
 
+void SemanticAnalyser::analyseArrayDeclaration(ArrayDeclarationNode* node, Context& ctx) {
+    if (node->elements) {
+        ctx.initialised_vars.insert(node->identifier);
+        analyseExpression(node->elements.get(), ctx);
+    }
+}
+
 void SemanticAnalyser::analyseVariableAssignment(VariableAssignmentNode* node, Context& ctx) {
+    analyseExpression(node->value.get(), ctx);
+    ctx.initialised_vars.insert(node->identifier);
+}
+
+void SemanticAnalyser::analyseArrayAssignment(ArrayAssignmentNode* node, Context& ctx) {
+    Symbol* array = table.lookup(node->identifier);
+    uint64_t array_size = array->array_size;
+    
+    if (auto* p = dynamic_cast<NumberLiteralNode*>(node->index.get())) {
+        if (p->value >= array_size) {
+            error("array index " + std::to_string(p->value) + " is out of bounds for array '" + node->identifier + "' of size " + std::to_string(array_size), node->line, node->column);
+        }
+        else if (p->value < 0) {
+            error("array index " + std::to_string(p->value) + " is negative for array '" + node->identifier + "'", node->line, node->column);
+        }
+    }
+
     analyseExpression(node->value.get(), ctx);
     ctx.initialised_vars.insert(node->identifier);
 }
 
 void SemanticAnalyser::analyseReturn(ReturnNode* node, Context& ctx) {
     if (ctx.return_type == Type::VOID && node->expression) {
-        // error
+        error("void function '" + ctx.name + "' cannot return a value", node->line, node->column);
     }
     else if (ctx.return_type != Type::VOID && !node->expression) {
-        // error
+        error("non-void function '" + ctx.name + "' must return a value", node->line, node->column);
     }
 
     if (node->expression) {
@@ -148,11 +172,64 @@ void SemanticAnalyser::analyseExpression(ASTNode* node, Context& ctx) {
     else if (auto* p = dynamic_cast<FunctionCallNode*>(node)) {
         analyseFunctionCall(p, ctx);
     }
+    else if (auto* p = dynamic_cast<BinaryOperationNode*>(node)) {
+        analyseBinOp(p, ctx);
+    }
+    else if (auto* p = dynamic_cast<UnaryOperationNode*>(node)) {
+        analyseUnaryOp(p, ctx);
+    }
+    else if (auto* p = dynamic_cast<ArrayAccessNode*>(node)) {
+        analyseArrayAccess(p, ctx);
+    }
+    else if (auto* p = dynamic_cast<ArrayLiteralNode*>(node)) {
+        analyseArrayLiteral(p, ctx);
+    }
 }
 
 void SemanticAnalyser::analyseIdentifier(IdentifierNode* node, Context& ctx) {
     if (ctx.initialised_vars.find(node->value) == ctx.initialised_vars.end()) {
-        // error
+        error("variable '" + node->value + "' may be used before initialisation", node->line, node->column);
+    }
+}
+
+void SemanticAnalyser::analyseBinOp(BinaryOperationNode* node, Context& ctx) {
+    if (auto* p = dynamic_cast<NumberLiteralNode*>(node->right.get())) {
+        if (node->operation == "/" && p->value == 0) {
+            error("division by zero", node->line, node->column);
+        }
+    }
+
+    analyseExpression(node->left.get(), ctx);
+    analyseExpression(node->right.get(), ctx);
+}
+
+void SemanticAnalyser::analyseUnaryOp(UnaryOperationNode* node, Context& ctx) {
+    analyseExpression(node->operand.get(), ctx);
+}
+
+void SemanticAnalyser::analyseArrayAccess(ArrayAccessNode* node, Context& ctx) {
+    if (auto* p = dynamic_cast<NumberLiteralNode*>(node->index.get())) {
+        Symbol* array = table.lookup(node->identifier);
+        uint64_t array_size = array->array_size;
+        if (p->value >= array_size) {
+            error("array index " + std::to_string(p->value) + " is out of bounds for array '" + node->identifier + "' of size " + std::to_string(array_size), node->line, node->column);
+        }
+
+        else if (p->value < 0) {
+            error("array index " + std::to_string(p->value) + " is negative for array '" + node->identifier + "'", node->line, node->column);
+        }
+
+        else if (ctx.initialised_vars.find(node->identifier) == ctx.initialised_vars.end()) {
+            error("array '" + node->identifier + "' may be used before initialisation", node->line, node->column);
+        }
+    }
+
+    analyseExpression(node->index.get(), ctx);
+}
+
+void SemanticAnalyser::analyseArrayLiteral(ArrayLiteralNode* node, Context& ctx) {
+    for (auto& it : node->value) {
+        analyseExpression(it.get(), ctx);
     }
 }
 
